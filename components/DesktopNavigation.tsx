09f3284991a747c4f40592246b468c40df2c6fbe
mirsazzathossain <mirsazzathossain@gmail.com>
2022-11-25 23:53:42 +0600

    refactor: :recycle: update Navigation component
    -- update navigation component name to DesktopNavigation
    -- refactor the codes to make more readable and moduler

---
+++ components/DesktopNavigation.tsx
@@ -0,0 +1,54 @@
+import clsx from "clsx";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { UrlObject } from "url";
+
+function NavItem({
+  href,
+  children,
+}: {
+  href: UrlObject | string;
+  children: React.ReactNode;
+}) {
+  let isActive = useRouter().pathname === href;
+
+  return (
+    <li>
+      <Link
+        href={href}
+        className={clsx(
+          "relative block px-3 py-2 transition",
+          isActive
+            ? "text-teal-500 dark:text-teal-400"
+            : "hover:text-teal-500 dark:hover:text-teal-400"
+        )}
+      >
+        {children}
+        {isActive && (
+          <span className="absolute inset-x-1 -bottom-px h-px bg-gradient-to-r from-teal-500/0 via-teal-500/40 to-teal-500/0 dark:from-teal-400/0 dark:via-teal-400/40 dark:to-teal-400/0" />
+        )}
+      </Link>
+    </li>
+  );
+}
+
+export default function DesktopNavigation({
+  links,
+  props,
+}: {
+  links: { href: UrlObject | string; label: string }[];
+  props: React.HTMLAttributes<HTMLDetailsElement>;
+}): JSX.Element {
+  let router = useRouter();
+  return (
+    <nav {...props}>
+      <ul className="flex rounded-full bg-white/90 px-3 text-sm font-medium text-zinc-800 shadow-lg shadow-zinc-800/5 ring-1 ring-zinc-900/5 backdrop-blur dark:bg-zinc-800/90 dark:text-zinc-200 dark:ring-white/10">
+        {links.map(({ href, label }, index) => (
+          <NavItem key={index} href={href}>
+            {label}
+          </NavItem>
+        ))}
+      </ul>
+    </nav>
+  );
+}
