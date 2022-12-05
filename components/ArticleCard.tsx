9072dc6ee6976e37274c2867f574616e1ab8a829
mirsazzathossain <mirsazzathossain@gmail.com>
2022-12-05 17:10:43 +0600

    feat: :sparkles: add article card component

---
+++ components/ArticleCard.tsx
@@ -0,0 +1,30 @@
+import { Card } from "./Card";
+
+export default function ArticleCard({ article }: { article: any }) {
+  return (
+    <article className="md:grid md:grid-cols-4 md:items-baseline">
+      <Card className="md:col-span-3">
+        <Card.Title href={`/articles/${article.slug}`}>
+          {article.title}
+        </Card.Title>
+        <Card.Eyebrow
+          as="time"
+          dateTime={article.date}
+          className="md:hidden"
+          decorate
+        >
+          {article.date}
+        </Card.Eyebrow>
+        <Card.Description>{article.description}</Card.Description>
+        <Card.Cta>Read article</Card.Cta>
+      </Card>
+      <Card.Eyebrow
+        as="time"
+        dateTime={article.date}
+        className="mt-1 hidden md:block"
+      >
+        {article.date}
+      </Card.Eyebrow>
+    </article>
+  );
+}
