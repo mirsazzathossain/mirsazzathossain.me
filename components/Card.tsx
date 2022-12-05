62d6a06885144f017124e26dc4aa85e8d34ce2f8
mirsazzathossain <mirsazzathossain@gmail.com>
2022-12-05 17:13:34 +0600

    fix: :bug: update card component

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,21 +1,6 @@
 import clsx from "clsx";
 import Link from "next/link";
-import { SVGProps } from "react";
-
-function ChevronRightIcon(
-  props: JSX.IntrinsicAttributes & SVGProps<SVGSVGElement>
-): JSX.Element {
-  return (
-    <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" {...props}>
-      <path
-        d="M6.75 5.75 9.25 8l-2.5 2.25"
-        strokeWidth="1.5"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
+import { ChevronRightIcon } from "./Icons";
 
 export function Card({
   as: Component = "div",
@@ -30,6 +15,7 @@ export function Card({
     <Component
       className={clsx(className, "group relative flex flex-col items-start")}
     >
+      <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl" />
       {children}
     </Component>
   );
@@ -44,7 +30,6 @@ Card.Link = function CardLink({
 }): JSX.Element {
   return (
     <>
-      <div className="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl" />
       <Link href={""} {...props}>
         <span className="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl" />
         <span className="relative z-10">{children}</span>
