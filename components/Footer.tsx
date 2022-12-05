b842cdf8137583a4cc19a1e389d2b330c682e081
mirsazzathossain <mirsazzathossain@gmail.com>
2022-12-05 17:09:47 +0600

    feat: :sparkles: add footer component

---
+++ components/Footer.tsx
@@ -0,0 +1,45 @@
+import { Container } from "components/Container";
+import Link from "next/link";
+import SpotifyPlayingNow from "./SpotifyPlayingNow";
+
+function NavLink({
+  href,
+  children,
+}: {
+  href: string;
+  children: React.ReactNode;
+}) {
+  return (
+    <Link
+      href={href}
+      className="transition hover:text-teal-500 dark:hover:text-teal-400"
+    >
+      {children}
+    </Link>
+  );
+}
+
+export default function Footer() {
+  return (
+    <footer className="mt-32">
+      <SpotifyPlayingNow />
+      <Container.Outer>
+        <div className="border-t border-zinc-100 pt-10 pb-16 dark:border-zinc-700/40">
+          <Container.Inner>
+            <div className="flex flex-col items-center justify-between gap-6 sm:flex-row">
+              <div className="flex gap-6 text-sm font-medium text-zinc-800 dark:text-zinc-200">
+                <NavLink href="/about">About</NavLink>
+                <NavLink href="/projects">Projects</NavLink>
+                <NavLink href="/speaking">Speaking</NavLink>
+                <NavLink href="/uses">Uses</NavLink>
+              </div>
+              <p className="text-sm text-zinc-400 dark:text-zinc-500">
+                © {new Date().getFullYear()} all rights reserved.
+              </p>
+            </div>
+          </Container.Inner>
+        </div>
+      </Container.Outer>
+    </footer>
+  );
+}
