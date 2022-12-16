ea006e21c90e0ad5c7e9ebded9ee1e4cc22bb586
mirsazzathossain <mirsazzathossain@gmail.com>
2022-12-17 05:21:24 +0600

    fix: :bug: add key to components

--- components/skeleton/LifeEventsPlaceholder.tsx
+++ components/skeleton/LifeEventsPlaceholder.tsx
@@ -7,19 +7,19 @@ export default function LifeEventsPlaceholder() {
         <div className="h-[26px] bg-gray-300 rounded-full dark:bg-gray-600 w-[150px] md:h-[32px] md:w-[200px]" />
 
         {[...Array(3)].map((_, i) => (
-          <>
+          <div key={i}>
             <div className="h-4 bg-gray-300 rounded-full dark:bg-gray-600 w-[100px] mb-8 mt-9" />
 
             {[...Array(2)].map((_, j) => (
-              <>
+              <div key={j}>
                 <div className="h-2 bg-gray-300 rounded-full dark:bg-gray-600 w-[200px] mb-6 ml-4" />
                 <div className="space-y-3 ml-8 mb-8">
                   <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-full" />
                   <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-600 w-1/2" />
                 </div>
-              </>
+              </div>
             ))}
-          </>
+          </div>
         ))}
       </div>
     </Container>
