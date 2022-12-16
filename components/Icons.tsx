3c8c8abe7b01c301cc5ac9eb9ee7a6acd4bbf757
mirsazzathossain <mirsazzathossain@gmail.com>
2022-12-17 05:17:24 +0600

    fix: :bug: update a html arribute name to work on tsx

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -101,7 +101,7 @@ export function DownloadFileIcon(
     <svg
       stroke="currentColor"
       fill="currentColor"
-      stroke-width="0"
+      strokeWidth="0"
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
       {...props}
