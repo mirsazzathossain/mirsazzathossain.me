0ff7c3b6aaf8cf8baf0259dc4f5c2a1bfb43a032
mirsazzathossain <mirsazzathossain@gmail.com>
2023-02-16 05:24:15 +0600

    style: :lipstick: update step style and inline code style

--- components/mdx/Step.tsx
+++ components/mdx/Step.tsx
@@ -6,7 +6,7 @@ export default function Step({
   title: string;
 }): JSX.Element {
   return (
-    <div className="step flex items-center py-4">
+    <div className="step flex items-center mt-8">
       <div className="flex items-center justify-center font-bold rounded-full h-8 w-8 text-teal-500 border border-zinc-200 dark:border-zinc-700/50">
         {number}
       </div>
