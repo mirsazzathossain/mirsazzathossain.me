dec29cd910d5bddbb1e66b19ff3454a3313f9041
Mir Sazzat Hossain <mirsazzathossain@gmail.com>
2023-11-18 02:42:54 +0600

    Fix cv download error

--- app/About.tsx
+++ app/About.tsx
@@ -41,7 +41,9 @@ function SocialLink({
       href={""}
       className="group -m-1 p-1"
       target={"_blank"}
+      rel="noopener noreferrer"
       locale={false}
+      download
       {...props}
     >
       <Icon className="h-6 w-6 fill-zinc-500 transition group-hover:fill-zinc-600 dark:fill-zinc-400 dark:group-hover:fill-zinc-300" />
@@ -121,7 +123,6 @@ export default async function About() {
             href={about.resume}
             title="Download Resume"
             icon={DownloadFileIcon}
-            rel="noopener noreferrer"
           />
         </div>
       </div>
