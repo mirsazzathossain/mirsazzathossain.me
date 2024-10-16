176528afaa9d4cde77660fa53f9ca7e88bca8dc2
mirsazzathossain@gmail.com <kishor25@g.skku.skku.edu>
2024-10-16 23:23:42 +0600

    refactor: Replace Card component with ProjectCard component in ListProjects

--- app/projects/ListProjects.tsx
+++ app/projects/ListProjects.tsx
@@ -1,8 +1,6 @@
-import { Card } from "components/Card";
-import { LinkIcon } from "components/Icons";
+import ProjectCard from "components/ProjectCard";
 import { server } from "config";
 import fs, { promises as ps } from "fs";
-import Image from "next/image";
 
 // get projects from local file
 async function getListProjects(): Promise<Project[]> {
@@ -29,26 +27,7 @@ export default async function ListProjects(): Promise<JSX.Element> {
       className="grid grid-cols-1 gap-x-12 gap-y-16 sm:grid-cols-2 lg:grid-cols-3"
     >
       {projects.map((project) => (
-        <Card as="li" key={project.title}>
-          <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-            <Image
-              src={`/images/${project.logo.src}`}
-              alt={project.logo.alt}
-              height={32}
-              width={32}
-              className="h-8 w-8"
-              unoptimized
-            />
-          </div>
-          <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
-            <Card.Link href={project.link.href}>{project.title}</Card.Link>
-          </h2>
-          <Card.Description>{project.description}</Card.Description>
-          <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-            <LinkIcon className="h-6 w-6 flex-none" />
-            <span className="ml-2">{project.link.label}</span>
-          </p>
-        </Card>
+        <ProjectCard project={project} key={project.title} />
       ))}
     </ul>
   );
---
+++ components/ProjectCard.tsx
@@ -0,0 +1,32 @@
+import { Card } from "components/Card";
+import { LinkIcon } from "components/Icons";
+import Image from "next/image";
+
+export default function ProjectCard({
+  project,
+}: {
+  project: Project;
+}): JSX.Element {
+  const [isImageLoading, setImageLoading] = React.useState(true);
+  return (
+    <Card as="li">
+      <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md shadow-zinc-800/5 ring-1 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
+        <Image
+          src={`/images/${project.logo.src}`}
+          alt={project.logo.alt}
+          height={32}
+          width={32}
+          className={`${isImageLoading ? "hidden" : "block"} h-8 w-8`}
+        />
+      </div>
+      <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
+        <Card.Link href={project.link.href}>{project.title}</Card.Link>
+      </h2>
+      <Card.Description>{project.description}</Card.Description>
+      <p className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
+        <LinkIcon className="h-6 w-6 flex-none" />
+        <span className="ml-2">{project.link.label}</span>
+      </p>
+    </Card>
+  );
+}
