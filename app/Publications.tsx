61dc20f242ea325578e47c81fc316fc094d8c0cb
mirsazzathossain@gmail.com <kishor25@g.skku.skku.edu>
2024-12-01 06:02:01 +0600

    refactor: adjust height calculation in Publications component and remove month from bibliography entry

--- app/Publications.tsx
+++ app/Publications.tsx
@@ -33,9 +33,7 @@ export default function Publications({
                 }
                 ref={parentRef as any}
                 style={{
-                  height: isExpanded
-                    ? (parentRef.current as any).scrollHeight
-                    : 0,
+                  height: isExpanded ? fit-content : 0,
                 }}
               >
                 <PublicationCard key={index} publication={publication} />
